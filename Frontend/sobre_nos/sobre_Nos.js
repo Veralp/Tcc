fa043a10c8dab1a3e41930bb28af90f709aaf8d9
Veralp <veradcj.fl@hotmail.com>
2023-11-10 08:07:47 -0300

    updates

--- Frontend/sobre_nos/sobre_Nos.js
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector('.btn')
-div.onclick = function() {
-  document.documentElement.classList.toggle('clean')
-  if(div.innerText == 'CLEAN') {
-    div.innerText = 'WASH'  
-  } else {
-    div.innerText = 'CLEAN'
-  }
-}
